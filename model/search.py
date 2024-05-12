05461910d76378b86566ec891caea961204d06ad
Wenxiao1210 <142988789+Wenxiao1210@users.noreply.github.com>
2024-05-12 19:40:01 +0800

    Add files via upload
    基本检索和歌词检索

--- model/search.py
+++ model/search.py
@@ -1,7 +1,9 @@
 import jieba
 from musicdata import db
 cur = db.cursor()
-def search_results(keyword):
+
+#歌名
+def songsearch_results(keysong):
     search_result = []
     # # 取出待搜索keyword
     # keyword = request.form['keyword']
@@ -12,13 +14,69 @@ def search_results(keyword):
     #  search_result.extend(sql_query(cut_keyword))
     # 记录搜到了多少数据
     #search_result = set(search_result)
-
-    search_result.extend(sql_query(keyword))
+    search_result.extend(sql_query(keysong))
     return search_result
-
-def sql_query(keyword):
-    sql= "select SongID,SongName,Duration,Version,AlbumID from songs where SongName like '%%%%%s%%%%'" % keyword
+def sql_query(keysong):
+    sql= "select SongID,SongName,Duration,Version,AlbumID from songs where SongName like '%%%%%s%%%%'" % keysong
     cur.execute(sql)
     result=cur.fetchall()
     return result
+
+#歌手
+def artistsearch_results(keyartist):
+    search_result = []
+    search_result.extend(sql_artistquery(keyartist))
+    return search_result
+def sql_artistquery(key_artist):
+    # 参数化的 SQL 查询，使用 JOIN 来从 performances 和 songs 表中获取相关的歌曲信息
+    sql = """
+    SELECT s.SongID, s.SongName, s.Duration, s.Version, s.AlbumID
+    FROM artists a
+    JOIN performances p ON a.ArtistID = p.ArtistID
+    JOIN songs s ON p.SongID = s.SongID
+    WHERE a.ArtistName LIKE %s
+    """
+    # 使用 %s 作为占位符，之后传递一个包含 '%' + key_artist + '%' 的元组来安全地填充占位符
+    cur.execute(sql, ('%' + key_artist + '%',))
+    result = cur.fetchall()
+    return result
+
+#专辑
+def albumsearch_results(keyalbum):
+    search_result = []
+    search_result.extend(sql_albumquery(keyalbum))
+    return search_result
+
+def sql_albumquery(key_album):
+    # 参数化的 SQL 查询，使用 JOIN 来从 albums 和 songs 表中获取相关的歌曲信息
+    sql = """
+    SELECT s.SongID, s.SongName, s.Duration, s.Version, s.AlbumID
+    FROM albums a
+    JOIN songs s ON a.AlbumID = s.AlbumID
+    WHERE a.AlbumName LIKE %s
+    """
+    # 使用 %s 作为占位符，之后传递一个包含 '%' + key_album + '%' 的元组来安全地填充占位符
+    cur.execute(sql, ('%' + key_album + '%',))
+    result = cur.fetchall()
+    return result
+
+#版本
+
+def versionsearch_results(key_version):
+    search_result = []
+    search_result.extend(sql_versionquery(key_version))
+    return search_result
+
+def sql_versionquery(key_version):
+    # 参数化的 SQL 查询，从 songs 表中获取特定版本的歌曲信息
+    sql = """
+    SELECT SongID, SongName, Duration, Version, AlbumID
+    FROM songs
+    WHERE Version LIKE %s
+    """
+    # 使用 %s 作为占位符，之后传递一个包含 '%' + key_version + '%' 的元组来安全地填充占位符
+    cur.execute(sql, ('%' + key_version + '%',))
+    result = cur.fetchall()
+    return result
+
 
