3be9b9a9282f08f3eba942f0f1c9f236b5187014
Wenxiao1210 <142988789+Wenxiao1210@users.noreply.github.com>
2024-05-14 22:37:14 +0800

    5.14合并歌词和基本检索

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from model.search import artistsearch_results
 from model.search import sql_artistquery
 from model.search import albumsearch_results
 from model.search import sql_albumquery
-from model.whoosh_test import whoosh_search,whoosh_index
 from musicdata import db
 from model.Jieba_query import inverted_index
 app = Flask(__name__)
@@ -56,22 +55,12 @@ def register():
     return render_template('register.html')
 
 
-@app.route("/lyrics_search", methods=["GET","POST"])
-def lyrics_search():
-    if request.args.get('key_word', None) == None:  # 如果没有检测到关键字提交，就停留在检索页面
-        print("未传参")
-        return render_template("search.html")  # 映射到检索页面
-    else:  # 如果有关键词提交
-        key_words = request.args.get('key_word')  # 将传来的关键词赋给key_word
-        keywords = key_words.split()
-        search_results1 = model.Jieba_query.show_results(keywords, inverted_index)  # 在表里查询符合条件的条目赋给key_words
-        whoosh_index()
-        search_results2 = whoosh_search(keywords)
-        return render_template("results.html", search_results1=search_results1,search_results2=search_results2)  # 映射到结果的页面，并将查询到的条目传过去
 
-@app.route("/search", methods=["GET"])
+@app.route("/search", methods=["GET","POST"])
 def search():
     search_params = {}
+    if request.args.get('key_word'):
+        search_params['word'] = request.args.get('key_word').strip()
     if request.args.get('key_artist'):
         search_params['artist'] = request.args.get('key_artist').strip()
     if request.args.get('key_song'):
@@ -80,11 +69,17 @@ def search():
         search_params['album'] = request.args.get('key_album').strip()
     if request.args.get('key_version'):
         search_params['version'] = request.args.get('key_version').strip()
+
     if not search_params:
         return render_template("search.html", error="请至少填写一个搜索条件。")
 
     # 初始结果集为空列表
     results_sets = []
+    if 'word' in search_params:
+        key_words = request.args.get('key_word')  # 将传来的关键词赋给key_words
+        keywords = key_words.split()
+        search_results = model.Jieba_query.show_results(keywords, inverted_index)  # 在表里查询符合条件的条目赋给key_words
+        results_sets.append(set(search_results))  # 将结果添加到结果集列表中
     if 'artist' in search_params:
         artist_results = set(model.search.artistsearch_results(search_params['artist']))
         results_sets.append(artist_results)
