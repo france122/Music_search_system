fd874631f5ee1376919044d6924252a6ccb1978b
Wenxiao1210 <142988789+Wenxiao1210@users.noreply.github.com>
2024-05-17 20:49:17 +0800

    517关联collect

--- data_files/music_user.sql
+++ data_files/music_user.sql
@@ -24,8 +24,9 @@ DROP TABLE IF EXISTS `user`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `username` varchar(50) NOT NULL,
-  `password` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`username`)
+  `password` varchar(30) NOT NULL,
+  PRIMARY KEY (`username`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -48,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-16 15:57:22
+-- Dump completed on 2024-05-17 20:47:53
