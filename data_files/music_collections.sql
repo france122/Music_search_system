eb252f15f67412cbe0f09018507b4da91218085b
Wenxiao1210 <142988789+Wenxiao1210@users.noreply.github.com>
2024-05-18 16:30:26 +0800

    518更新songs、collections、performances

--- data_files/music_collections.sql
+++ data_files/music_collections.sql
@@ -16,28 +16,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `user`
+-- Table structure for table `collections`
 --
 
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `collections`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
+CREATE TABLE `collections` (
   `username` varchar(20) NOT NULL,
-  `password` varchar(15) NOT NULL,
+  `SongID` char(5) NOT NULL,
   PRIMARY KEY (`username`),
-  UNIQUE KEY `username_UNIQUE` (`username`)
+  KEY `FK_collections_SongID_idx` (`SongID`),
+  CONSTRAINT `FK_collections_SongID` FOREIGN KEY (`SongID`) REFERENCES `songs` (`SongID`),
+  CONSTRAINT `FK_collections_username` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `collections`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('admin','123456');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+LOCK TABLES `collections` WRITE;
+/*!40000 ALTER TABLE `collections` DISABLE KEYS */;
+/*!40000 ALTER TABLE `collections` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
