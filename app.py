149489aa1e74f9e3af0702d51628fc301000620f
Wenxiao1210 <142988789+Wenxiao1210@users.noreply.github.com>
2024-05-21 23:26:44 +0800

    更正返回

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, jsonify, session
 import model.search
 from model.check_login import is_existed, exist_user, is_null
 from model.check_regist import add_user
-from model.search import songsearch_results, emotionsearch_results,artistsearch_results, albumsearch_results, versionsearch_results, get_song_details, get_song_detail_by_id, get_artist_biography
+from model.search import (songsearch_results, emotionsearch_results, artistsearch_results,
+                          albumsearch_results, versionsearch_results, get_song_details,
+                          get_song_detail_by_id, get_artist_biography)
 import time
 from musicdata import db, cursor
-from flask import jsonify,session
-from model.whoosh_test import whoosh_search,whoosh_index
+from model.whoosh_test import whoosh_search
 from model.Jieba_query import inverted_index
-
 
 app = Flask(__name__)
 app.secret_key = '1234'
+
 @app.route('/')
 def index():
     return redirect(url_for('user_login'))
-
-
-
 
 @app.route('/user_login', methods=['GET', 'POST'])
 def user_login():
@@ -29,7 +27,7 @@ def user_login():
             return render_template('login.html', message=login_message)
         elif is_existed(username, password):
             session['username'] = username
-            return render_template('index.html', username=username)
+            return redirect(url_for('index1'))
         elif exist_user(username):
             login_message = "温馨提示：密码错误，请输入正确密码"
             return render_template('login.html', message=login_message)
@@ -38,7 +36,7 @@ def user_login():
             return render_template('login.html', message=login_message)
     return render_template('login.html')
 
-@app.route("/register", methods=["GET", 'POST'])
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -50,17 +48,16 @@ def register():
             login_message = "温馨提示：用户已存在，请直接登录"
             return render_template('register.html', message=login_message)
         else:
-            add_user(request.form['username'], request.form['password'])
-            return render_template('index.html', username=username)
+            add_user(username, password)
+            return redirect(url_for('index1'))
     return render_template('register.html')
 
-
-@app.route('/test_search', methods=['GET','POST'])
+@app.route('/test_search', methods=['GET', 'POST'])
 def test_search():
     key_word = request.args.get('key_word')  # 从GET请求中获取key_word
-    key_word=key_word.split()
-    search_results1, search_results2,time1,time2 = lyrics_search(key_word)
-    return render_template('compare.html', search_results1=search_results1,search_results2=search_results2,time1=time1,time2=time2)
+    key_word = key_word.split()
+    search_results1, search_results2, time1, time2 = lyrics_search(key_word)
+    return render_template('compare.html', search_results1=search_results1, search_results2=search_results2, time1=time1, time2=time2)
 
 def lyrics_search(keywords):
     start_time1 = time.time()
@@ -73,29 +70,28 @@ def lyrics_search(keywords):
     time2 = round(end_time2 - start_time2, 3)
     detailed_results1 = get_song_details(search_results1)
     detailed_results2 = get_song_details(search_results2)
-    return detailed_results1,detailed_results2,time1,time2
+    return detailed_results1, detailed_results2, time1, time2
 
 @app.route('/compare')
 def compare():
     return render_template('compare.html')
 
-
-@app.route("/search", methods=["GET", "POST"])
+@app.route("/search", methods=["POST"])
 def search():
     search_params = {}
     artist_biography = None
     major_artists = ['苏打绿', '凤凰传奇', '周杰伦', '李宇春', '张学友', '五月天']
 
     # 获取搜索参数
-    fields = request.args.getlist('fields[]')
+    fields = request.form.getlist('fields[]')
     for field in fields:
-        if field and request.args.get(field):
-            search_params[field] = request.args.get(field).strip()
+        if field and request.form.get(field):
+            search_params[field] = request.form.get(field).strip()
             if field == 'key_artist' and search_params[field] in major_artists:
                 artist_biography = get_artist_biography(search_params[field])
 
-    arousal = request.args.get('arousal')
-    valence = request.args.get('valence')
+    arousal = request.form.get('arousal')
+    valence = request.form.get('valence')
 
     if not search_params and (arousal is None or valence is None):
         return render_template("index.html", error="请至少填写一个搜索条件。")
@@ -161,13 +157,17 @@ def search():
     # 从后端获取详细的歌曲信息
     detailed_results = get_song_details(final_results)
 
-    # 调试输出
-    # print("Search Parameters:", search_params)
-    # print("Final Results:", final_results)
-    # print("Detailed Results:", detailed_results)
+    # 使用 session 存储搜索结果
+    session['detailed_results'] = detailed_results
+    session['artist_biography'] = artist_biography
 
+    return redirect(url_for('results'))
+
+@app.route('/results')
+def results():
+    detailed_results = session.get('detailed_results', [])
+    artist_biography = session.get('artist_biography', None)
     return render_template("results.html", search_results=detailed_results, artist_biography=artist_biography)
-
 
 def all_field_search_results(keyword):
     # 对各个字段分别进行搜索并转换为集合
@@ -197,27 +197,22 @@ def song_detail(song_id):
     song_detail = get_song_detail_by_id(song_id)
     if not song_detail:
         return render_template("songdetail.html", error="找不到该歌曲的详细信息。")
-    # print(song_detail)
     return render_template("songdetail.html", song_detail=song_detail)
 
 @app.route('/index1')
 def index1():
-    username=session["username"]
-    return render_template('index.html',username=username)
+    username = session["username"]
+    return render_template('index.html', username=username)
 
 @app.route("/self")
 def self():
     myusername = session['username']
-    #myusername = request.form.get("username")
-    sql = "select SongID from collects where username = 'admin' "#.format(myusername=myusername)
+    sql = "select SongID from collects where username = 'admin'"
     cursor.execute(sql)
     collect_results = []
-    collect_results_songID= cursor.fetchall()
+    collect_results_songID = cursor.fetchall()
     collect_results = get_song_details(collect_results_songID)
-
-    # print(collect_results)
     return render_template("self.html", collect_results=collect_results)
-
 
 @app.route('/add_to_favorites/<song_id>', methods=['POST'])
 def add_to_favorites(song_id):
@@ -241,7 +236,7 @@ def remove_from_favorites(song_id):
         return jsonify({'message': 'Username is required'}), 400
     try:
         with db.cursor() as cursor:
-            sql = "DELETE FROM collects WHERE username=%s  AND SongID = %s"
+            sql = "DELETE FROM collects WHERE username=%s AND SongID = %s"
             cursor.execute(sql, (username, song_id))
 
         db.commit()  # 假设 db 是你的数据库连接对象
@@ -250,11 +245,5 @@ def remove_from_favorites(song_id):
         return jsonify({'message': 'Failed to remove song from collects: ' + str(e)}), 500
     return jsonify({'message': 'Song has been removed from collects'})
 
-
-
-
-
 if __name__ == '__main__':
-
     app.run(debug=True, host='0.0.0.0', port=6688)
-
